8a28210cf9faf8baa71d05d7e5b8a00cab5035d9
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2019-05-15 21:53:02 -0400

    Perform face detection via opencv and save to visitors

--- assistant/views.py
+++ assistant/views.py
@@ -1,5 +1,7 @@
 import base64
 import re
+import cv2
+import datetime
 from io import BytesIO
 
 from PIL import Image, ImageFilter
@@ -17,12 +19,33 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 
 
+CASCADE = "face_cascade.xml"
+FACE_CASCADE = cv2.CascadeClassifier(CASCADE)
+
+
+def opencv_face_detection(filename: str) -> str:
+    image = cv2.imread(filename)
+    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = FACE_CASCADE.detectMultiScale(image_grey, scaleFactor=1.16, minNeighbors=5, minSize=(25, 25), flags=0)
+
+    for x, y, w, h in faces:
+        sub_img = image[y - 10:y + h + 10, x - 10:x + w + 10]
+        cv2.imwrite('visitors/' + str(datetime.datetime.now()).replace(':', '-') + ".jpg", sub_img)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
+
+    return opencv_image_to_base64(image)
+
+
 def detect_face(request):
+    filename = 'temp.jpg'
     context = {}
     imageBase64 = re.search(r'base64,(.*)', request.POST.get('imageBase64')).group(1)
-    img = save_image_from_base64(imageBase64, 'temp.jpg')
+    img = save_image_from_base64(imageBase64, filename)
     img = img.filter(ImageFilter.FIND_EDGES)
-    context['imgB64'] = 'data:image/jpeg;base64,' + PILimageToBase64(img)
+    opencv_face_detection(filename)
+    context['imgB64'] = 'data:image/jpeg;base64,' + opencv_face_detection(filename)
+    # context['imgB64'] = 'data:image/jpeg;base64,' + PIL_image_to_base64(img)
     context['status'] = 'success'
     return JsonResponse(context)
 
@@ -35,7 +58,12 @@ def save_image_from_base64(imageBase64: str, filename: str) -> Image:
     return img
 
 
-def PILimageToBase64(image: Image) -> str:
+def opencv_image_to_base64(image) -> str:
+    _, buffer = cv2.imencode('.jpg', image)
+    return base64.b64encode(buffer).decode('UTF-8')
+
+
+def PIL_image_to_base64(image: Image) -> str:
     buffered = BytesIO()
     image.save(buffered, format="JPEG")
     return base64.b64encode(buffered.getvalue()).decode('UTF-8')
